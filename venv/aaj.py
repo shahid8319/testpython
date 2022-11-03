b177643d1beb6017fda1a99eea8794070328dac1
shahid8319 <116978858+shahid8319@users.noreply.github.com>
2022-11-03 11:37:06 -0400

    Nov3

---
+++ venv/aaj.py
@@ -0,0 +1,4 @@
+name1 = input("enter your name!")
+name2 = input( " your last name !")
+city  = input (" tell me your city name !")
+print( "your name is:  " + name1  +"Your last name ! "+ name2 + " your city is 1: "+ city)
